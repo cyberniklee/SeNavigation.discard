9b3b3b4e20c296d13bedac813741aecdabc8c563
cyberniklee <cyberniklee@gmail.com>
2016-12-29 20:47:27 +0800

    add recovery behavior

---
+++ Source/Navigation/Planner/Base/RecoveryBehavior.h
@@ -0,0 +1,25 @@
+/*
+ * RecoveryBehavior.h
+ *
+ *  Created on: 2016年12月28日
+ *      Author: seeing
+ */
+
+#ifndef _RECOVERYBEHAVIOR_H_
+#define _RECOVERYBEHAVIOR_H_
+
+class RecoveryBehavior{
+public:
+	RecoveryBehavior(){};
+	virtual ~RecoveryBehavior(){};
+public:
+	void initialize();
+
+	virtual void onInitialize() = 0;
+
+	virtual void run() = 0;
+};
+
+
+
+#endif /* NAVIGATION_PLANNER_BASE_RECOVERYBEHAVIOR_H_ */
