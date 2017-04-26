b59d171b47290573f15cfa87ac80be8323f22c99
cyberniklee <cyberniklee@gmail.com>
2017-04-26 18:51:05 +0800

    p

--- Build/Source/Controller/Communication/subdir.mk
+++ Build/Source/Controller/Communication/subdir.mk
@@ -20,7 +20,7 @@ CPP_DEPS += \
 Source/Controller/Communication/%.o: ../Source/Controller/Communication/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	arm-openwrt-linux-muslgnueabi-g++ -D__GLIBC__ -I$(SENAVICOMMON_PATH)/Source -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	arm-openwrt-linux-muslgnueabi-g++ -I$(SENAVICOMMON_PATH)/Source -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Build/Source/Navigation/CostMap/subdir.mk
+++ Build/Source/Navigation/CostMap/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 Source/Navigation/CostMap/%.o: ../Source/Navigation/CostMap/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -D__GLIBC__ -I$(SENAVICOMMON_PATH)/Source -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -I$(SENAVICOMMON_PATH)/Source -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
